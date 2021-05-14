1a76569512f274b0a97a205b040fa2514397adb6
DSarceno <dsarceno68@gmail.com>
2021-05-14 12:17:50 -0600

    header

---
+++ SimuC/head.c
@@ -0,0 +1,31 @@
+//   2021-01-28
+//   <program>.f95
+//   Diego Sarceño (dsarceno68@gmail.com)
+
+//   Programa <program>
+
+//   Codificación del texto: UTF8
+//   Compiladores probados: GNU Fortran (Ubuntu 20.04 Linux) 4.8.5
+//   Instrucciones de compilación: no requiere nada mas
+//   gfortran -Wall -pedantic -std=f95 -c -o <program>.o <program>.f95
+//   gfortran -o <program>.x <program>.o 
+
+//   Copyright (C) 2021
+//   D. R. Sarceño Ramírez
+//   dsarceno68@gmail.com
+
+//   This program is free software: you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation, either version 3 of
+//   the License, or (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   General Public License for more details.
+
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see
+//   <http://www.gnu.org/licenses/>.
+
